b7b1c250baf9da5bb5d76ec04ffbc9880e40696f
BFSmx4 <97541925+BFSmx4@users.noreply.github.com>
2022-03-07 15:00:54 -0600

    Update Transaction.js

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -28,8 +28,8 @@ export default class TransactionScreen extends Component {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
 
     this.setState({
-      /*status === "granted" is true when user has granted permission
-          status === "granted" is false when user has not granted the permission
+      /*status === "granted" es verdadero cuando es concedido el permiso.
+          status === "granted" es falso cuando es concedido el permiso.
         */
       hasCameraPermissions: status === "granted",
       domState: domState,
@@ -80,7 +80,7 @@ export default class TransactionScreen extends Component {
                 style={styles.scanbutton}
                 onPress={() => this.getCameraPermissions("bookId")}
               >
-                <Text style={styles.scanbuttonText}>Scan</Text>
+                <Text style={styles.scanbuttonText}>Escanear</Text>
               </TouchableOpacity>
             </View>
             <View style={[styles.textinputContainer, { marginTop: 25 }]}>
@@ -94,7 +94,7 @@ export default class TransactionScreen extends Component {
                 style={styles.scanbutton}
                 onPress={() => this.getCameraPermissions("studentId")}
               >
-                <Text style={styles.scanbuttonText}>Scan</Text>
+                <Text style={styles.scanbuttonText}>Escanear</Text>
               </TouchableOpacity>
             </View>
           </View>
